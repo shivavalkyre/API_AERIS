29d0f176a67b12f80f8d16195ee7804e7a29bdf4
syerif elfaz <syerif@gmail.com>
2023-12-04 18:47:09 +0700

    test2

--- controllers/geofence.js
+++ controllers/geofence.js
@@ -420,54 +420,6 @@ var history = async function(req,res){
       }
 }
 
-var history_detail = async function(req,res){
-  try {
-    var token = process.env.TOKEN_AERTRAK
-
-    var url = process.env.URL_GEOFENCE_HISTORY_DETAILS + "startDate=" + req.body.startDate + "&endDate=" + req.body.endDate;
-
-    futil.logger.debug('\n' + futil.shtm() + '- [ URL ] | INFO ' + util.inspect(url));
-    futil.logger.debug('\n' + futil.shtm() + '- [ REQ BODY ] | INFO ' + util.inspect(req.body));
-
-    const config = {
-      headers:{
-          token : process.env.TOKEN_AERTRAK
-      }
-    }
-
-          axios.get(url,config) .then(function (response) {
-
-            futil.logger.debug('\n' + futil.shtm() + '- [ RESPONSE BODY ] | INFO ' + util.inspect(response.data));
-
-            var result = {
-                "status":true,
-                "message":"success",
-                "data": response.data
-              }
-              res.setHeader("Content-Type", "application/json");
-              res.writeHead(200);
-              res.end(JSON.stringify(result));
-          })
-          .catch(function (err) {
-            var result = {  
-
-                "status":false,
-                "message": err
-            }
-            res.setHeader("Content-Type", "application/json");
-            res.writeHead(400);
-            res.end(JSON.stringify(result,null,3));
-          })
-
-
-      }catch (err) {
-        futil.logger.debug('\n' + futil.shtm() + '- [ ERROR ] | QUERING ' + util.inspect(err));
-        result.code = 400
-        result.status ="failed"
-        result.data = "Read data failed"
-        res.send(result);
-      }
-}
 
 module.exports = {
     Create,
