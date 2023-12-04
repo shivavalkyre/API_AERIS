5839d8ff9cf20d9baef5c2f5a4d3e4b1a128c449
syerif elfaz <syerif@gmail.com>
2023-12-04 16:23:13 +0700

    test

--- controllers/geofence.js
+++ controllers/geofence.js
@@ -371,10 +371,105 @@ var Delete = async function(req,res){
 }
 
 
+var History = async function(req,res){
+  try {
+    var token = process.env.TOKEN_AERTRAK
+
+    var url = process.env.URL_GEOFENCE_HISTORY+"startDate="+ req.body.start_date + "&endDate=" + req.body.endDate;
+
+    futil.logger.debug('\n' + futil.shtm() + '- [ URL ] | INFO ' + util.inspect(url));
+    futil.logger.debug('\n' + futil.shtm() + '- [ REQ BODY ] | INFO ' + util.inspect(req.body));
+
+    const config = {
+        headers:{
+            token : token
+        }
+      }
+
+      axios.get(url,config) .then(function (response) {
+
+        futil.logger.debug('\n' + futil.shtm() + '- [ RESPONSE BODY ] | INFO ' + util.inspect(response.data));
+
+        var result = {
+            "status":true,
+            "message":"success",
+            "data": response.data
+          }
+          res.setHeader("Content-Type", "application/json");
+          res.writeHead(200);
+          res.end(JSON.stringify(result));
+      })
+      .catch(function (err) {
+        var result = {  
+
+            "status":false,
+            "message": err
+        }
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(400);
+        res.end(JSON.stringify(result,null,3));
+      })
+  }catch(err){
+    futil.logger.debug('\n' + futil.shtm() + '- [ ERROR ] | QUERING ' + util.inspect(err));
+    result.code = 400
+    result.status ="failed"
+    result.data = "Read data failed"
+    res.send(result);
+  }
+}
+
+var HistoryDetail = async function(req,res){
+    try {
+      var token = process.env.TOKEN_AERTRAK
+
+      var url = process.env.URL_GEOFENCE_HISTORY+"startDate="+ req.body.start_date + "&endDate=" + req.body.endDate + "&sclId=" + req.body.sclId;
+
+      futil.logger.debug('\n' + futil.shtm() + '- [ URL ] | INFO ' + util.inspect(url));
+      futil.logger.debug('\n' + futil.shtm() + '- [ REQ BODY ] | INFO ' + util.inspect(req.body));
+
+      const config = {
+          headers:{
+              token : token
+          }
+        }
+
+        axios.get(url,config) .then(function (response) {
+
+          futil.logger.debug('\n' + futil.shtm() + '- [ RESPONSE BODY ] | INFO ' + util.inspect(response.data));
+
+          var result = {
+              "status":true,
+              "message":"success",
+              "data": response.data
+            }
+            res.setHeader("Content-Type", "application/json");
+            res.writeHead(200);
+            res.end(JSON.stringify(result));
+        })
+        .catch(function (err) {
+          var result = {  
+
+              "status":false,
+              "message": err
+          }
+          res.setHeader("Content-Type", "application/json");
+          res.writeHead(400);
+          res.end(JSON.stringify(result,null,3));
+        })
+    }catch(err){
+      futil.logger.debug('\n' + futil.shtm() + '- [ ERROR ] | QUERING ' + util.inspect(err));
+      result.code = 400
+      result.status ="failed"
+      result.data = "Read data failed"
+      res.send(result);
+    }
+}
+
 module.exports = {
     Create,
     Read,
     Update,
-    Delete
-
+    Delete,
+    History,
+    HistoryDetail
 }
