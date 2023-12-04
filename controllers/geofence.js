487f3d725e72a84e2364c40cbad7a3c838244492
syerif elfaz <syerif@gmail.com>
2023-12-04 17:19:57 +0700

    test

--- controllers/geofence.js
+++ controllers/geofence.js
@@ -381,41 +381,43 @@ var history = async function(req,res){
     futil.logger.debug('\n' + futil.shtm() + '- [ REQ BODY ] | INFO ' + util.inspect(req.body));
 
     const config = {
-        headers:{
-            token : token
-        }
+      headers:{
+          token : process.env.TOKEN_AERTRAK
       }
+    }
 
-      axios.get(url,config) .then(function (response) {
+          axios.get(url,config) .then(function (response) {
 
-        futil.logger.debug('\n' + futil.shtm() + '- [ RESPONSE BODY ] | INFO ' + util.inspect(response.data));
+            futil.logger.debug('\n' + futil.shtm() + '- [ RESPONSE BODY ] | INFO ' + util.inspect(response.data));
 
-        var result = {
-            "status":true,
-            "message":"success",
-            "data": response.data
-          }
-          res.setHeader("Content-Type", "application/json");
-          res.writeHead(200);
-          res.end(JSON.stringify(result));
-      })
-      .catch(function (err) {
-        var result = {  
+            var result = {
+                "status":true,
+                "message":"success",
+                "data": response.data
+              }
+              res.setHeader("Content-Type", "application/json");
+              res.writeHead(200);
+              res.end(JSON.stringify(result));
+          })
+          .catch(function (err) {
+            var result = {  
 
-            "status":false,
-            "message": err
-        }
-        res.setHeader("Content-Type", "application/json");
-        res.writeHead(400);
-        res.end(JSON.stringify(result,null,3));
-      })
-  }catch(err){
-    futil.logger.debug('\n' + futil.shtm() + '- [ ERROR ] | QUERING ' + util.inspect(err));
-    result.code = 400
-    result.status ="failed"
-    result.data = "Read data failed"
-    res.send(result);
-  }
+                "status":false,
+                "message": err
+            }
+            res.setHeader("Content-Type", "application/json");
+            res.writeHead(400);
+            res.end(JSON.stringify(result,null,3));
+          })
+
+
+      }catch (err) {
+        futil.logger.debug('\n' + futil.shtm() + '- [ ERROR ] | QUERING ' + util.inspect(err));
+        result.code = 400
+        result.status ="failed"
+        result.data = "Read data failed"
+        res.send(result);
+      }
 }
 
 var history_detail = async function(req,res){
