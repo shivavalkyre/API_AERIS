f6b23005bdbd11ccafbbe1343cba0ae1731b2b25
syerif elfaz <syerif@gmail.com>
2023-12-04 19:08:06 +0700

    test2

--- routes/index.js
+++ routes/index.js
@@ -180,9 +180,6 @@ router.post('/api/patern/geofence/history',Auth.authAccessToken,function(req,res
 })
 
 
-// router.post('/api/patern/geofence/detail',Auth.authAccessToken,function(req,res){
-//     Geofence.history_detail(req,res)
-// })
 
 
 router.get('/api/patern/geofence',Auth.authAccessToken,function(req,res){
