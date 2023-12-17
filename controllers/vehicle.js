fa3f530a3cdede78bf0a1c5bf47b5fefb09fd57c
syerif elfaz <syerif@gmail.com>
2023-12-17 07:31:27 +0700

    vehicle selected

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -225,7 +225,7 @@ var ReadSelected = async function (req,res){
         futil.logger.debug('\n' + futil.shtm() + '- [ REQ PARAMSS] | INFO ' + util.inspect(req.params));
         futil.logger.debug('\n' + futil.shtm() + '- [ REQ BODY] | INFO ' + util.inspect(req.body));
 
-        var vehicleid = req.headers.vehicleid
+        var vehicleid = req.params.vehicleid
 
         const count = await Vehicle.count({
             where: {
