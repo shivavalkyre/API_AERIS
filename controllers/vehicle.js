5d10b46115ba7e00065f0f58a76d28c12fe03c6c
syerif elfaz <syerif@gmail.com>
2023-12-26 17:43:49 +0700

    update

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -266,7 +266,7 @@ var ReadCategory = async function (req,res){
             // futil.logger.debug('\n' + futil.shtm() + '- [ RESP] | INFO ' + util.inspect(resp)); 
 
             loop1: for (i=0;i<=data_length-1;i++){
-                futil.logger.debug('\n' + futil.shtm() + '- [ DATA ] | INFO ' + util.inspect(data[i].vin)); 
+                futil.logger.debug('\n' + futil.shtm() + '- [ DATA '+ i +'] | INFO ' + util.inspect(data[i].vin)); 
                 loop2: for (j=0;j<=resp.length-1;j++){
                     // futil.logger.debug('\n' + futil.shtm() + '- [ RESP] | INFO ' + util.inspect(resp[j])); 
                     
